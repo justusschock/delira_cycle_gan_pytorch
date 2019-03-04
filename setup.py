5023ab1f98d9f9361d2fd3f5ec8d96be42795918
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-03-04 11:30:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ def find_version(file):
     if version_match:
         return version_match.group(1)
 
-
+requirements = resolve_requirements(os.path.join(os.path.dirname(__file__),
+                                                 'requirements.txt'))
 readme = read_file(os.path.join(os.path.dirname(__file__), "README.md"))
 license = read_file(os.path.join(os.path.dirname(__file__), "LICENSE"))
 cycle_gan_version = find_version(os.path.join(os.path.dirname(__file__),
@@ -44,5 +45,16 @@ setup(
     license=license,
     author='Justus Schock',
     author_email='justus.schock@rwth-aachen.de',
-    description=''
+    install_requires=requirements,
+    tests_require=["pytest-cov"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Medical Science Apps."
+    ]
 )
